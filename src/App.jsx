bdd20a68e40ac0521aeb8bd9d6c8aa23dc1ac5cf
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-10 14:30:14 +0500

    Terminal added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Navbar from './components/Navbar.jsx'
 import Sidebar from './components/Sidebar.jsx'
-
+import Terminal from './components/Terminal.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar/>
-      <Sidebar/>
+      <div className='flex'>
+        <Sidebar/>
+        <div className='flex justify-center w-screen bg-blue-200'>
+          <Terminal/>
+        </div>
+        
+      </div>
+
     </>
   )
 }
