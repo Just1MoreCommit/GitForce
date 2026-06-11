f51b269e6c3b873c536d5fc9678a9360c2afad36
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 00:06:26 +0500

    feat: added pages and shifted landing page to home. keeping app seperate

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../components/Navbar.jsx"
+import Sidebar from "../components/Sidebar.jsx"
+import Terminal from "../components/Terminal.jsx"
+function Home() {
+
+  return (
+    <>
+      <Navbar/>
+      <div className='flex bg-[#000000]'>
+        <Sidebar/>
+        <div className='flex justify-center w-screen bg-[#000000] min-h-screen pt-[15vh]'>
+          <Terminal/>
+        </div>
+        
+      </div>
+
+      <div className="flex flex-col min-h-screen pb-10">
+  
+        <footer className="fixed bottom-0 w-full py-3 border-t border-[#2D2D2D] text-center font-mono bg-[#0D0D0D]">
+            <p className="text-[#444444] text-xs tracking-widest">
+                GITFORCE v1.0.0 &nbsp;|&nbsp; made with ♥ for devs, by a dev
+            </p>
+        </footer>
+  </div>
+
+    </>
+  )
+
+
+}
+
+
+export default Home
