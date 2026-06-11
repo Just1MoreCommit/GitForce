9a8a5cfdb766185630c1c035ea669da19067b8e5
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-11 23:04:28 +0500

    landing page complete, revamped ui: minimalistic, removed borders, added footer.

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ function App() {
   return (
     <>
       <Navbar/>
-      <div className='flex'>
+      <div className='flex bg-[#000000]'>
         <Sidebar/>
-        <div className='flex justify-center w-screen bg-[#1C1B1B] min-h-screen pt-[15vh]'>
+        <div className='flex justify-center w-screen bg-[#000000] min-h-screen pt-[15vh]'>
           <Terminal/>
         </div>
         
       </div>
+
+      <div className="flex flex-col min-h-screen pb-10">
+  
+        <footer className="fixed bottom-0 w-full py-3 border-t border-[#2D2D2D] text-center font-mono bg-[#0D0D0D]">
+            <p className="text-[#444444] text-xs tracking-widest">
+                GITFORCE v1.0.0 &nbsp;|&nbsp; made with ♥ for devs, by a dev
+            </p>
+        </footer>
+  </div>
 
     </>
   )
