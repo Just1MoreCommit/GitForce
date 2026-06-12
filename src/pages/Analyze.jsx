86d38e3e6868b9217d05e8f877595d13236e3c11
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 23:05:08 +0500

    react flow testing

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -1,7 +1,9 @@
 
-import Navbar from '../components/analyze/Navbar'
+import MiddleDisplay from '../components/analyze/MiddleDisplay';
 
-import SidebarLeft from '../components/analyze/SidebarLeft'
+import Navbar from '../components/analyze/Navbar';
+
+import SidebarLeft from '../components/analyze/SidebarLeft';
 
 import SidebarRight from '../components/analyze/SidebarRight';
 
@@ -11,6 +13,9 @@ function Analyze() {
             <Navbar/>
             <div className='h-screen flex justify-between'>
                 <SidebarLeft/>
+                <div className="flex-1" style={{ height: 'calc(100vh - 40px)' }}>
+                  <MiddleDisplay/>
+                </div>
                 <SidebarRight/>
             </div>
         </>
