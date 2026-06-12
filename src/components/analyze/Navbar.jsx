262843f26a63b235034c4fdac15aa11dc3e2572b
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 12:58:20 +0500

    feat: GitForce text added, settings icon, colors-[#000000]  added to Navbar

--- src/components/analyze/Navbar.jsx
+++ src/components/analyze/Navbar.jsx
@@ -1,7 +1,14 @@
 function Navbar() {
     return(
+        <div className="navbar bg-[#000000] flex justify-between items-center font-mono">
+            <p className="text-[#FACC15] p-4 text-[30px]">GITFORCE</p>
 
-    )
+            <div className="side flex gap-8 p-4">
+                <p className="text-[#FACC15]">⚙️</p>
+
+            </div>
+        </div>
+    );
 }
 
 export default Navbar
