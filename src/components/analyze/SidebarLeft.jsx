b5cdfa677923e837edcbb77fa3968652b0a94470
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 21:42:47 +0500

    feat: added left sidebar, added 3 buttons, styling: bg color and border right added

--- src/components/analyze/SidebarLeft.jsx
+++ src/components/analyze/SidebarLeft.jsx
@@ -1,6 +1,12 @@
 function SidebarLeft() {
     return(
-
+            <div className="sidebar flex flex-col bg-[#0e0e0e] border-r-2 border-[#1c1b1b] h-full w-[250px] font-mono">
+                <div className="flex flex-col items-center justify-evenly  h-[200px]">
+                    <button className="text-[#FACC15]"> Analysis </button>
+                    <button className="text-[#FACC15]"> Narrator </button>
+                    <button className="text-[#FACC15]"> Report </button>
+                </div>
+            </div>
     )
 }
 
