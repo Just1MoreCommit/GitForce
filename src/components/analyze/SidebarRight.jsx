1b52949f54fdf2063a655cbc448bf962c8228d5c
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 22:16:01 +0500

    added chat element atop the sidebar. Added styling

---
+++ src/components/analyze/SidebarRight.jsx
@@ -0,0 +1,11 @@
+function SidebarRight() {
+    return(
+        <div className="sidebar flex flex-col bg-[#0e0e0e] border-l-2 border-[#1c1b1b] h-full w-[250px] font-mono">
+                <div className="upperBox bg-[#000000] flex justify-center">
+                    <p className="bg-[#1a1400] text-[#FACC15] border border-[#FFE500] px-12 py-1 m-4">Chat</p>
+                </div>
+            </div>
+    )
+}
+
+export default SidebarRight
