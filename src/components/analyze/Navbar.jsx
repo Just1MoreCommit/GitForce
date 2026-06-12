133db8f18d349b5c7ce46c61655cdda53b55a12a
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 21:41:56 +0500

    added search field for commits, added commits button again, fixed spacing

--- src/components/analyze/Navbar.jsx
+++ src/components/analyze/Navbar.jsx
@@ -4,18 +4,18 @@ function Navbar() {
             
            
             
-            <p className="text-[#FACC15] pl-4 text-[20px]">GITFORCE</p>
-                <div className="navButtons flex gap-12 text-[#FACC15]">
+            <p className="text-[#FACC15] pl-6 text-[20px]">GITFORCE</p>
+                <div className="navButtons flex gap-6 text-[#FACC15]">
                     <button>Workspace</button>
                     <button>Commits</button>
                     <button>Agents</button>
-               </div> 
+                    </div> 
              
 
-            
-                <p className="text-[#FACC15] pr-4">⚙️</p>
-
-            
+            <div className="side flex justify-between gap-6 p-4">
+                <input type="text" placeholder="🔍 search for commits..." className="focus:outline focus:outline-[#000000] bg-[#0e0e0e] p-2 border-2 border-[#1c1b1b] text-white " />
+                <p className="text-[#FACC15] mt-2">⚙️</p>
+            </div>
         </div>
     );
 }
