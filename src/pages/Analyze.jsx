3505a878f4af9d0e45b9db6c180cc3612f5cf2c5
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-12 21:47:55 +0500

    right side bar implementation

--- src/pages/Analyze.jsx
+++ src/pages/Analyze.jsx
@@ -3,13 +3,15 @@ import Navbar from '../components/analyze/Navbar'
 
 import SidebarLeft from '../components/analyze/SidebarLeft'
 
+import SidebarRight from '../components/analyze/SidebarRight';
 
 function Analyze() {
     return(
         <>
             <Navbar/>
-            <div className='h-screen'>
+            <div className='h-screen flex justify-between'>
                 <SidebarLeft/>
+                <SidebarRight/>
             </div>
         </>
     );
